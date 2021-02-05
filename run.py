0ec29b43094e08a794cb14efbd4b2ecf5e2dae4b
Diego Escalante <diego.escalante01@gmail.com>
2021-02-04 21:08:50 -0700

    Revert "Change from cyan to blue", I changed my mind.
    This reverts commit af6db7dd2cb5f19ec635852ae21583b3386eeb09.

--- run.py
+++ run.py
@@ -90,14 +90,14 @@ def getColorForDay(year, month, day):
         elif status == "BAD":
             return Color.RED
         elif status == "NEUTRAL":
-            return Color.BLUE
+            return Color.CYAN
         else:
             print("WARNING! Unknown habit status for " + year + "-" + month + "-" + day, flush=True)
             return Color.YELLOW
 
     relativeTime = isPastPresentOrFuture(datetime.datetime(year=int(year), month=int(month), day=int(day)))
     if relativeTime == -1:
-        return Color.BLUE
+        return Color.CYAN
     elif relativeTime == 0:
         writeHabitData(selectedDay.year, selectedDay.month, selectedDay.day, "BAD")
         return Color.RED
@@ -201,9 +201,9 @@ def middlePressed():
         color = Color.RED
         writeHabitData(selectedDay.year, selectedDay.month, selectedDay.day, "BAD")
     elif color == Color.RED:
-        color = Color.BLUE
+        color = Color.CYAN
         writeHabitData(selectedDay.year, selectedDay.month, selectedDay.day, "NEUTRAL")
-    elif color == Color.BLUE:
+    elif color == Color.CYAN:
         color = Color.GREEN
         writeHabitData(selectedDay.year, selectedDay.month, selectedDay.day, "GOOD")
     leds[getLedForSelectedDay()] = color
